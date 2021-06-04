35ae56b09941bb052fd6d0d2d06fadcd020b3087
junho <junho6667@gmail.com>
2021-06-04 14:22:44 +0900

    2021/06/04

---
+++ src/main/java/me/junhoship/Masula.java
@@ -0,0 +1,21 @@
+package me.junhoship;
+
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.implementation.FixedValue;
+
+import java.io.File;
+import java.io.IOException;
+
+import static net.bytebuddy.matcher.ElementMatchers.named;
+
+public class Masula {
+    public static void main(String[] args) {
+        try {
+            new ByteBuddy().redefine(Moja.class)
+                    .method(named("pullOut")).intercept(FixedValue.value("Rabbit!"))
+                    .make().saveIn(new File("/target/classes/"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/me/junhoship/Moja.java
@@ -0,0 +1,10 @@
+package me.junhoship;
+
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.matcher.ElementMatchers;
+
+public class Moja {
+    public String pullOut(){
+        return "";
+    }
+}
